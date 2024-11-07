e0eca0029000286c3109868c468fb80d7c37aaae
Divye Negi <divyenegi@gmail.com>
2024-11-07 20:36:52 +0530

    changed build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,17 @@
 echo "BUILD START"
-python3.9 -m pip install -r requirements.txt
+
+python3 -m pip install -r requirements.txt
+
+echo "Installing tailwindcss and dependencies..."
+python3 manage.py tailwind install
+
+echo "Building tailwindcss..."
+python3 manage.py tailwind build
+
+echo "Migrating Database..."
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
+
+echo "Collecting static files..."
 python3.9 manage.py collectstatic --noinput --clear
 echo "BUILD END"
