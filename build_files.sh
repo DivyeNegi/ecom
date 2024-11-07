12d8c4e46c6ca3e6672ea3dd9ef706ae13f43779
Divye Negi <divyenegi@gmail.com>
2024-11-07 21:17:54 +0530

    changes

--- build_files.sh
+++ build_files.sh
@@ -2,12 +2,6 @@ echo "BUILD START"
 
 python3 -m pip install -r requirements.txt
 
-echo "Installing tailwindcss and dependencies..."
-python3 manage.py tailwind install
-
-echo "Building tailwindcss..."
-python3 manage.py tailwind build
-
 echo "Migrating Database..."
 python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
