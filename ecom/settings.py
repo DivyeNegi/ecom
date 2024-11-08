3052b98bcfdf6cd256caa271d080798ecef4f3d3
Divye Negi <divyenegi@gmail.com>
2024-11-08 21:44:41 +0530

    changes in static root

--- ecom/settings.py
+++ ecom/settings.py
@@ -120,10 +120,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_ROOT = 'staticfiles'
+# STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
 
 STATICFILES_DIR = {
     os.path.join(BASE_DIR, "public/static")
